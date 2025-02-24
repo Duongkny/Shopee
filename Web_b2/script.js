e87829301ce788d75412a68841f44c19e9c7a3ac
Tùng Dương <kaduong240@gmail.com>
2025-02-24 12:04:23 +0700

    Add files via upload

---
+++ Web_b2/script.js
@@ -0,0 +1,5 @@
+var cart=document.querySelector('.icon-shopping')
+var mess=document.querySelector('.header-nav-mess')
+cart.addEventListener('click',()=>{
+    mess.classList.toggle('header-nav-mess')
+})
